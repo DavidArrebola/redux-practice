7f74e07f0f570b25c59d6a5b2c35e790748026a5
David <davida@ambrosebuilding.com.au>
2021-07-19 19:44:42 +1000

    Added slices

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,12 @@
 import { useSelector, useDispatch } from "react-redux";
 
-import { counterActions } from "../store";
+import { counterActions } from "../store/counter-slice";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
-  const show = useSelector((state) => state.showCounter);
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
 
   const incrementHandler = () => {
     dispatch(counterActions.increment());
@@ -30,7 +30,7 @@ const Counter = () => {
       {show && <div className={classes.value}> {counter} </div>}{" "}
       <div>
         <button onClick={incrementHandler}>Increment</button>
-        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
